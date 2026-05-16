15f60cf94af1a2c3e7b885214612d7b978344d1d
Skytale97 <crystalmeghan800@gmail.com>
2026-05-16 12:42:41 -0300

    Add files via upload

---
+++ clientes.py
@@ -0,0 +1,36 @@
+import pandas as pd
+try:
+    df = pd.read_csv('dados_salvos.csv', encoding='latin-1', sep=';')
+    df['Idade'] = df['Idade'].astype(int)
+    df['Renda'] = df['Renda'].astype(str).str.replace('.', '').astype(float)
+
+    df['Media'] = df[['Idade', 'Renda']].mean(axis=1)
+
+    def cidade_mais_frequente(df):
+        contagem = df['Cidade'].value_counts()
+        cidade_principal = contagem.idxmax()
+        return cidade_principal
+    
+    def filtrar_clientes(df):
+        rendas_altas = df[df['Renda'] >= 5000]
+        rendas_medias = df[df['Renda'].between(2000, 3500)]
+        rendas_baixas =df[df['Renda'] <= 2000]
+        return rendas_altas, rendas_medias, rendas_baixas
+
+    print("Sucesso ao ler como CSV!")
+    print("-" * 30)
+    print(df)
+    print("-" * 30)
+    print(f"A cidade mais freqeunte é {cidade_mais_frequente(df)}")
+    print("-" * 30)
+    rendas_altas, rendas_medias, rendas_baixas = filtrar_clientes(df)
+    for nome in rendas_altas['Nome']:
+        print(f"  - {nome}")    
+    print("\n Pessoas com Renda Média:")
+    for nome in rendas_medias['Nome']:
+        print(f"  - {nome}")
+    print("\n Pessoas com Renda Baixa:")
+    for nome in rendas_baixas['Nome']:
+        print(f"  - {nome}")
+except Exception as e:
+    print(f'Erro ao ler o arquivo, tente novamente')
